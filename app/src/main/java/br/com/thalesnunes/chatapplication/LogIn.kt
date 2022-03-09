489b22f085ba74b6eb91e5b90b0d1f940b63f58f
Thales Nunes <thales.o.nunes@gmail.com>
2022-03-09 14:27:32 -0300

    Configurando botão que alterna para tela de cadastro

--- app/src/main/java/br/com/thalesnunes/chatapplication/LogIn.kt
+++ app/src/main/java/br/com/thalesnunes/chatapplication/LogIn.kt
@@ -1,11 +1,47 @@
 package br.com.thalesnunes.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LogIn : AppCompatActivity() {
+
+    // Declarando as variáveis necessárias
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        // Referenciando os elementos
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        // Redirecionando para a tela de SignUp
+        btnSignUp.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
