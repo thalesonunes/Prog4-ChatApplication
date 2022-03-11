f6d8faf2289267f1e503941dd229503680f5218a
--global <advresoares@yahoo.com.br>
2022-03-10 21:14:31 -0300

    Elementos da tela de mensagens

---
+++ app/src/main/java/br/com/thalesnunes/chatapplication/Message.kt
@@ -0,0 +1,13 @@
+package br.com.thalesnunes.chatapplication
+
+class Message {
+    var message: String? = null
+    var senderId: String? = null
+
+    constructor(){}
+
+    constructor(message: String?, senderId: String?){
+        this.message = message
+        this.senderId = senderId
+    }
+}
